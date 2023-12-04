48c7e254d3d20bd8a78078dfd2225cd1c82c22ea
Everton Ferreira dos Santos <evrttn@gmail.com>
2023-12-04 11:10:58 -0300

    uso de alarmOne e alarmTwo

--- irrigador.ino
+++ irrigador.ino
@@ -1,36 +1,24 @@
-/*
- * 
- * melhorias TODO
- * -no máximo 2 alarmes
- * -usar AlarmOne e AlarmTwo no setup
- * -translate variables to english
- * 
- * 
- */
- 
 #include <avr/sleep.h>
 #include <Wire.h>
 #include <RtcDS3231.h>
 #include "prescaler.h"
 RtcDS3231<TwoWire> Rtc(Wire);
 
-#define interruptPin 3
-#define irrigationInterval 15000 //15 s
-
-const uint8_t hora[] = {9,14,19};
-const uint8_t minutos[] = {0,0,0};
-const uint8_t num_alarmes = 3;
-uint8_t i = 0;
+#define INTERRUPT_PIN 3
+#define IRRIGATION_INTERVAL 15000 //15 s
+#define HOUR_ALARM_ONE 7
+#define HOUR_ALARM_TWO 19
 
 volatile bool irrigationMode = false;
  
 void setup()
 {
-  pinMode(interruptPin, INPUT_PULLUP);
+  pinMode(INTERRUPT_PIN, INPUT_PULLUP);
 
   Rtc.Begin();
   Rtc.Enable32kHzPin(false);
-  Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeAlarmOne); //Habilta alarme One
+  Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeAlarmBoth); //enables alarms One and Two
+  setWakeUpAlarms();
 
   DDRB |= 0x01; // same as pinMode(8, OUTPUT);
   PORTB |= 0x00; //same as digitalWrite(8, LOW);
@@ -38,23 +26,27 @@ void setup()
   setClockPrescaler(CLOCK_PRESCALER_256);
 
   while(true){ //avoid entering loop() because it always checks serial comm.
+    goToSleep();
     work();
   }
 }
 
-void work()
-{
-    uint8_t k = i++ % num_alarmes;
-    DS3231AlarmOne alarm1(0, hora[k], minutos[k], 0, DS3231AlarmOneControl_HoursMinutesSecondsMatch);
+void setWakeUpAlarms(){
+    DS3231AlarmOne alarm1(0, HOUR_ALARM_ONE, 0, 0, DS3231AlarmOneControl_HoursMinutesSecondsMatch);
     Rtc.SetAlarmOne(alarm1);
-    Rtc.LatchAlarmsTriggeredFlags();// Efetiva os alarmes
+
+    DS3231AlarmTwo alarm2(0, HOUR_ALARM_TWO, 0, DS3231AlarmTwoControl_HoursMinutesMatch);
+    Rtc.SetAlarmTwo(alarm2);
     
-    goToSleep();
+    Rtc.LatchAlarmsTriggeredFlags();// trigger alarms
+}
+
+void work()
+{ 
+  pumpWater();
+  Rtc.LatchAlarmsTriggeredFlags();// allows for alarms to trigger again
+  irrigationMode = false; //reset the flag
   
-    if(irrigationMode){
-        pumpWater();    
-        irrigationMode = false;
-    }
 }
 
 void wakeUp()
@@ -66,16 +58,16 @@ void goToSleep()
 {
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);  
   sleep_enable();    
-  attachInterrupt(digitalPinToInterrupt(interruptPin), wakeUp, LOW);
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), wakeUp, LOW);
   sleep_cpu();
   sleep_disable();
-  detachInterrupt(digitalPinToInterrupt(interruptPin));          
+  detachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN));          
 }
 
 void pumpWater()
 {
   PORTB |= 0x01;  
-  trueDelay(irrigationInterval);
+  trueDelay(IRRIGATION_INTERVAL);
   PORTB |= 0x00; 
 }
 

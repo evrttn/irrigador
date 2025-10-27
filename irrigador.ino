f8af29ef701f29c6fe720761a4a7790dcbd670bd
evertons <evertons@unicamp.br>
2025-10-27 00:42:37 -0300

    o código de producao crreto

--- irrigador.ino
+++ irrigador.ino
@@ -1,22 +1,12 @@
 #include <avr/sleep.h>
 #include <Wire.h>
 #include <RtcDS3231.h>
-#include "prescaler.h"
-#include "UART.h"
+#include <EEPROM.h>
+//#include "prescaler.h"
 
 #define BLUETOOTH_PIN 2
 #define INTERRUPT_PIN 3
-#define HOUR_ALARM_ONE 7
-#define HOUR_ALARM_TWO 21
-#define MAX_BUFF_SIZE  38
-
-RtcDS3231<TwoWire> Rtc(Wire);
-
-volatile bool irrigationMode = false;
-volatile bool bleMode = false;
-volatile bool myloop = true;
-
-uint16_t irrigInterval = 2000; //7.5s
+#define MAX_BUFF_SIZE 38
 
 volatile char UARTBUFF[MAX_BUFF_SIZE]={' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',
                    ' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',
@@ -24,139 +14,114 @@ volatile char UARTBUFF[MAX_BUFF_SIZE]={' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',
                    ' ',' ',' ',' ',' ',' ',' ',' '};
 
 volatile uint8_t addr = 0;
+volatile bool msgComplete = false;
+volatile bool bleMode = false;
+volatile bool irrigMode = false;
+
+RtcDS3231<TwoWire> Rtc(Wire);
+
+uint16_t irrigInterval;
 
 ISR(USART_RX_vect){
   char c = UDR0;
   
   if(addr >= MAX_BUFF_SIZE)
     addr = 0;
-
+    
   if(c == '!'){
-      myloop = false;
+      msgComplete = true;
       UARTBUFF[addr] = '\0';
       addr = 0;
   }else
-    UARTBUFF[addr++] = c;
+    UARTBUFF[addr++] = c;    
 }
 
-
-void tratarHorario(char * comm){
-  char *hora, *minuto;
-  strcpy(hora, strtok(comm, ":"));
-  strcpy(minuto, strtok(NULL, ":"));
-  
-  uint8_t h = atoi(hora);
-  uint8_t m = atoi(minuto);
-
-  RtcDateTime dateTime = Rtc.GetDateTime();
-  dateTime.SetHour(h);
-  dateTime.SetMinute(m);
-  Rtc.SetDateTime(dateTime);
-}
-
-void tratarAlarmeOne(char * comm){
-  char *hora, *minuto;
-  
-  hora = strtok(comm, ":");
-  minuto = strtok(NULL, ":");
-  
-  uint8_t h = atoi(hora);
-  uint8_t m = atoi(minuto);
-
-  DS3231AlarmOne alarm1(0, h, m, 0, DS3231AlarmOneControl_HoursMinutesSecondsMatch);
-  Rtc.SetAlarmOne(alarm1);
-  Rtc.LatchAlarmsTriggeredFlags();// trigger alarms
-}
-
-void tratarAlarmeTwo(char * comm){
-  char *hora, *minuto;
-  
-  hora = strtok(comm, ":");
-  minuto = strtok(NULL, ":");
-  
-  uint8_t h = atoi(hora);
-  uint8_t m = atoi(minuto);
-
-  DS3231AlarmTwo alarm2(0, h, m, DS3231AlarmTwoControl_HoursMinutesMatch);
-  Rtc.SetAlarmTwo(alarm2);
-    
-  Rtc.LatchAlarmsTriggeredFlags();
-}
-
-void tratarData(char * comm){
-  char *dia, *mes, *ano;
-  dia = strtok(comm, "/");
-  mes = strtok(NULL, "/");
-  ano = strtok(NULL, "/");
-
-  uint8_t d = atoi(dia);
-  uint8_t m = atoi(mes);
-  uint16_t a = atoi(ano);
-
-  RtcDateTime dateTime = Rtc.GetDateTime();
-  dateTime.SetDay(d);
-  dateTime.SetMonth(m);
-  dateTime.SetYear(a);
-  Rtc.SetDateTime(dateTime);
-}
-
-void tratarTempo(char * comm){
-  uint16_t t = atoi(comm);
-  irrigInterval = t;  
-}
-
-#define countof(a) (sizeof(a) / sizeof(a[0]))
-
-void printDateTime(const RtcDateTime& dt)
+void wakeUp()
 {
-    char datestring[20];
-
-    snprintf_P(datestring, 
-            countof(datestring),
-            PSTR("%02u/%02u/%04u %02u:%02u:%02u"),
-            dt.Day(),
-            dt.Month(),
-            dt.Year(),
-            dt.Hour(),
-            dt.Minute(),
-            dt.Second() );
-    UART0_enviaString(datestring);
+  irrigMode = true;
 }
 
-void menu(char* comm){
-  char c = comm[0];
-  switch(c){
-    case 'H':
-      tratarHorario(comm+1);
-      break;
-    case 'A':
-      tratarAlarmeOne(comm+1);
-      break;
-    case 'B':
-      tratarAlarmeTwo(comm+1);
-      break;
-    case 'D':
-      tratarData(comm+1);
-      break;
-    case 'T':
-      tratarTempo(comm+1);
-      break;
-    case 'P':
-      RtcDateTime now = Rtc.GetDateTime();
-      printDateTime(now);
-      break;
-    default:
-      break;
-  }
+void wakeUpBLE()
+{
+  bleMode = !bleMode;
+  if(bleMode)
+    PORTB |= (1 << PB4);
+  else
+    PORTB &= ~(1 << PB4);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  UART0_config();
+  //sei(); desnecessario
+  DDRD &= ~((1 << DDD3) | (1 << PD2));
+  DDRD |= (1 << DDD4);
+  
+  PORTD |= ((1 << PD3) | (1 << PD2));
+  PORTD &= ~(1 << PD4);
+  PORTB &= ~(1 << PB4);
+
+  Rtc.Begin();
+  Rtc.Enable32kHzPin(false);
+  Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeAlarmBoth); //enables alarms One and Two
+  
+  uint8_t t = EEPROM.read(0);
+  irrigInterval = t * 1000;
+  
+  setWakeUpAlarms();  
+  
+  attachInterrupt(digitalPinToInterrupt(BLUETOOTH_PIN), wakeUpBLE, FALLING); //  
+}
+
+void setWakeUpAlarms()
+{
+  setWakeUpAlarmOne();
+  setWakeUpAlarmTwo();
+}
+
+void setWakeUpAlarmOne()
+{
+  uint8_t hOne = EEPROM.read(2);
+  uint8_t mOne = EEPROM.read(3);
+  
+    DS3231AlarmOne alarm1(0, hOne, mOne, 0, DS3231AlarmOneControl_HoursMinutesSecondsMatch);
+    Rtc.SetAlarmOne(alarm1);    
+}
+
+void setWakeUpAlarmTwo()
+{
+  uint8_t hTwo = EEPROM.read(4);
+  uint8_t mTwo = EEPROM.read(5);
+
+  DS3231AlarmTwo alarm2(0, hTwo, mTwo, DS3231AlarmTwoControl_HoursMinutesMatch);
+  Rtc.SetAlarmTwo(alarm2);
+}
+
+void goToSleep()
+{
+  Rtc.LatchAlarmsTriggeredFlags();// trigger alarms
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);  
+  sleep_enable();    
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), wakeUp, FALLING); //
+  sleep_cpu();
+  sleep_disable();
+  detachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN));
+}
+
+void loop() {
+   if(bleMode)
+    handleBLEMessage();
+   else{  
+    goToSleep();   
+    pumpWater();
+   }
 }
 
 void handleBLEMessage() {
-  if(myloop)
+  
+  if(!msgComplete)
     return;
 
-  bleMode = false;
-  
-  UART0_enviaString("handling ble");
   char *p[5];
   int i = 0;
   
@@ -180,101 +145,173 @@ void handleBLEMessage() {
       comm = p[j];
       menu(comm);
       j++;
-    }     
-  PORTB &= ~(1 << PB4);
-  UART0_enviaString("fim handling ble");
-   
+    }
+    msgComplete = false;
 }
- 
-void setup()
-{
-  UART0_config(); //BLE communication 
-  
-  //pinMode(INTERRUPT_PIN, INPUT_PULLUP);
-  DDRD &= ~((1 << DDD3) | (1 << DDD2));
-  PORTD |= ((1 << PD3) | (1 << PD2));
 
-  Rtc.Begin();
-  Rtc.Enable32kHzPin(false);
-  Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeAlarmBoth); //enables alarms One and Two
-  setWakeUpAlarms();
-
-  DDRD |= (1 << DDD4); // same as pinMode(4, OUTPUT);
-  PORTD &= ~(1 << PD4); //same as digitalWrite(4, LOW);
-
-  PORTB &= ~(1 << PB4);
-
-  setClockPrescaler(CLOCK_PRESCALER_256);
-
-  //sei();
-
-  while(true){ //avoid entering loop() because it always checks serial comm.
-    goToSleep();    
-    work();
+void menu(char* comm){
+  char c = comm[0];
+  switch(c){
+    case 'H':
+      tratarHorario(comm+1);
+      break;
+    case 'A':
+      tratarAlarmeOne(comm+1);
+      setWakeUpAlarmOne();
+      break;
+    case 'B':
+      tratarAlarmeTwo(comm+1);
+      setWakeUpAlarmTwo();
+      break;
+    case 'D':
+      tratarData(comm+1);
+      break;
+    case 'T':
+      tratarTempo(comm+1);
+      break;
+    case 'P':      
+      printDateTime();
+      break;
+    default:
+      break;
   }
 }
 
-void setWakeUpAlarms(){
-    DS3231AlarmOne alarm1(0, HOUR_ALARM_ONE, 0, 0, DS3231AlarmOneControl_HoursMinutesSecondsMatch);
-    Rtc.SetAlarmOne(alarm1);
+#define countof(a) (sizeof(a) / sizeof(a[0]))
 
-    DS3231AlarmTwo alarm2(0, HOUR_ALARM_TWO, 0, DS3231AlarmTwoControl_HoursMinutesMatch);
-    Rtc.SetAlarmTwo(alarm2);
+void printDateTime()
+{
+    uint8_t t = EEPROM.read(0);
     
-    Rtc.LatchAlarmsTriggeredFlags();// trigger alarms
+    RtcDateTime dt = Rtc.GetDateTime();
+    DS3231AlarmOne aOne = Rtc.GetAlarmOne();
+    DS3231AlarmTwo aTwo = Rtc.GetAlarmTwo();
+    
+    char datestring[43];
+
+    snprintf_P(datestring, 
+            countof(datestring),
+            PSTR("%02u/%02u/%04u %02u:%02u:%02u A %02u:%02u B %02u:%02u T %d"),
+            dt.Day(),
+            dt.Month(),
+            dt.Year(),
+            dt.Hour(),
+            dt.Minute(),
+            dt.Second(),
+            aOne.Hour(),
+            aOne.Minute(),
+            aTwo.Hour(),
+            aTwo.Minute(),
+            t);
+    
+  uint8_t i=0;
+  while (datestring[i] != '\0')
+  {
+    UART0_enviaCaractere(datestring[i++]);
+  }    
 }
 
-void work()
-{   
-  //sei(); do not work
-  if(irrigationMode)
-    pumpWater();
+void tratarHorario(char * comm){
+  char *hora, *minuto;
+
+  hora = strtok(comm, ":");
+  minuto = strtok(NULL, ":");
+   
+  uint8_t h = atoi(hora);
+  uint8_t m = atoi(minuto);
+
+  RtcDateTime dateTime = Rtc.GetDateTime();
+  dateTime.SetHour(h);
+  dateTime.SetMinute(m);
+  Rtc.SetDateTime(dateTime);
+}
+
+void tratarAlarmeOne(char * comm){
+  char *hora, *minuto;
   
-  while(bleMode)
-    handleBLEMessage();
-    
-  Rtc.LatchAlarmsTriggeredFlags();// allows for alarms to trigger again
-  irrigationMode = false; //reset the flag
-  myloop = true;
+  hora = strtok(comm, ":");
+  minuto = strtok(NULL, ":");
+  
+  uint8_t h = atoi(hora);
+  uint8_t m = atoi(minuto);
+
+  EEPROM.write(2, h);
+  EEPROM.write(3, m);
+  
 }
 
-
-void wakeUpBLE()
-{
-  bleMode = true;
-  PORTB |= (1 << PB4);
-  //UART0_enviaString("hello, Everton"); not connected yet  
+void tratarAlarmeTwo(char * comm){
+  char *hora, *minuto;
+  
+  hora = strtok(comm, ":");
+  minuto = strtok(NULL, ":");
+  
+  uint8_t h = atoi(hora);
+  uint8_t m = atoi(minuto);
+  
+  EEPROM.write(4, h);
+  EEPROM.write(5, m);
 }
 
-void wakeUp()
-{
-  irrigationMode = true;    
+void tratarData(char * comm){
+  char *dia, *mes, *ano;
+  dia = strtok(comm, "/");
+  mes = strtok(NULL, "/");
+  ano = strtok(NULL, "/");
+
+  uint8_t d = atoi(dia);
+  uint8_t m = atoi(mes);
+  uint16_t a = atoi(ano);
+
+  RtcDateTime dateTime = Rtc.GetDateTime();
+  dateTime.SetDay(d);
+  dateTime.SetMonth(m);
+  dateTime.SetYear(a);
+  Rtc.SetDateTime(dateTime);
 }
 
-void goToSleep()
-{
-  cli();
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);  
-  sleep_enable();    
-  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), wakeUp, FALLING); //
-  attachInterrupt(digitalPinToInterrupt(BLUETOOTH_PIN), wakeUpBLE, FALLING); //  
-  sei();
-  sleep_cpu();
-  sleep_disable();
-  sei();
-  //detachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN));
-  //detachInterrupt(digitalPinToInterrupt(BLUETOOTH_PIN));
-  //sei(); do not work
+void tratarTempo(char * comm){
+  uint8_t t = atoi(comm);
+  irrigInterval = t*1000;
+
+  EEPROM.write(0, t);
 }
 
 void pumpWater()
 {
-  PORTD |= (1 << PD4);  
-  trueDelay(irrigInterval);
-  PORTD &= ~(1 << PD4); 
+  if(irrigMode){
+    PORTD |= (1 << PD4);  
+    delay(irrigInterval);
+    PORTD &= ~(1 << PD4);
+    irrigMode = false;
+  } 
 }
 
-void loop()
-{  
+void UART0_config()
+{
+  //Baud Rate de 9600bps para um cristal de 16MHz (Datasheet)
+  UBRR0 = 103;
+  
+  //Habilita a interrupcao de recepcao e os pinos TX e RX
+  UCSR0B =  (1<<RXEN0) | (1<<TXEN0) | (1<<RXCIE0) ;
+  
+  //Configura a UART com 8 bits de dados
+  UCSR0C =  (1<<UCSZ01) | (1<<UCSZ00);
+}
 
+void UART0_enviaCaractere(unsigned char ch)
+{
+  UDR0 = ch;
+
+  //Aguarda o buffer ser desocupado
+  while (! (UCSR0A & (1<<UDRE0)) );
+}
+
+void UART0_enviaString(char *s)
+{
+  unsigned int i=0;
+  while (s[i] != '\0')
+  {
+    UART0_enviaCaractere(s[i++]);
+  }
 }
